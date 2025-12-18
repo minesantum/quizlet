52d27f0331da037b69b2858c2727c8b5f09b2bc9
MineSantum <minesantum@gmail.com>
2025-12-18 14:03:25 +0100

    es portátil

--- INICIAR_APP.bat
+++ INICIAR_APP.bat
@@ -4,11 +4,21 @@ echo ---------------------------------------------------
 echo   QuizClone - Tu Aplicacion de Flashcards
 echo ---------------------------------------------------
 echo.
-echo   [1] Iniciando servidor de datos...
-echo   [2] Tus datos se guardan automaticamente en database.json
+echo   [1] Buscando entorno PHP...
+
+set "PHP_BIN=php"
+if exist "%~dp0bin\php\php.exe" (
+    echo   [2] Usando PHP Portable...
+    set "PHP_BIN=%~dp0bin\php\php.exe"
+) else (
+    echo   [2] Usando PHP del Sistema...
+)
+
+echo   [3] Iniciando servidor de datos...
+echo   [4] Tus datos se guardan automaticamente en database.json
 echo.
 echo   Por favor, NO CIERRES esta ventana mientras estudias.
 echo.
 
 start http://localhost:8000
-php -S localhost:8000 router.php
+"%PHP_BIN%" -S localhost:8000 router.php
